360017610e15aacf61a74d3797d97a355a6a4f71
Dimitri Bobkov <bobkov.dimitri@gmail.com>
2022-04-11 01:28:37 +0100

    Clippy and fmt ftw

--- src/core/event_bus.rs
+++ src/core/event_bus.rs
@@ -3,64 +3,63 @@
 use super::Event;
 use super::Subscriber;
 
-
 /// # Event Bus
-/// 
+///
 /// The event bus is a central hub for all events.
 /// It is responsible for managing all subscribers and publishing events
 /// related to the event bus.
-/// 
+///
 /// ## Fields
-/// 
+///
 /// * `events` - A vec of events that have been published to the event bus.
-/// 
+///
 /// * `subscribers` - A vec of all subscribers to the event bus.
-/// 
+///
 /// ## Methods
-/// 
+///
 /// * `publish` - Publishes an event to the event bus.
-/// 
+///
 /// * `subscribe_listener` - Subscribes a listener to the event bus.
-/// 
+///
 /// * `run` - Runs through each event, and calls each listener's on_event method.
-/// 
+///
 /// * `clear` - Clears all events from the event bus.
 pub struct EventBus<T> {
     /// A vec of events that have been published to the event bus.
     events: Vec<Event<T>>,
     /// A vec of all subscribers that are linked to the event bus.
-    subscribers: Vec<Box<dyn Subscriber<Input = T>>>
+    subscribers: Vec<Box<dyn Subscriber<Input = T>>>,
 }
 
 impl<T> EventBus<T> {
     /// # New
-    /// 
+    ///
     /// Creates a new event bus.
     pub fn new() -> EventBus<T> {
         EventBus {
             events: Vec::new(),
-            subscribers: Vec::new()
+            subscribers: Vec::new(),
         }
     }
 
     /// # Publish
-    /// 
+    ///
     /// Publishes an event to the event bus.
     pub fn publish(&mut self, message: Event<T>) {
         self.events.push(message);
     }
 
     /// # Subscribe Listener
-    /// 
+    ///
     /// Subscribes a listener to the event bus.
-    pub fn subscribe_listener<R: Subscriber<Input = T> +'static>(&mut self, listener: R) {
+    pub fn subscribe_listener<R: Subscriber<Input = T> + 'static>(&mut self, listener: R) {
         self.subscribers.push(Box::new(listener));
     }
 
     /* Upon run, messages will be cleared! */
 
     /// # Run
-    /// 
+    ///
     /// Runs through each event, and calls each listener's on_event method.
     pub fn run(&mut self) {
         for event in self.events.drain(..) {
@@ -71,7 +70,7 @@ impl<T> EventBus<T> {
     }
 
     /// # Clear
-    /// 
+    ///
     /// Clears all events from the event bus.
     pub fn clear(&mut self) {
         self.events.clear();
--- src/core/subscriber.rs
+++ src/core/subscriber.rs
@@ -1,21 +1,21 @@
 use super::Event;
 
 /// # Subscriber
-/// 
+///
 /// A subscriber is a listener that is linked to an event bus.
 /// This is defined by the Subscriber trait, which can be implemented
 /// by any type that wants to subscribe to an event bus.
-/// 
+///
 /// The subscriber will receive an event when the event bus is run.
 /// It will be called through the on_event method, where it
 /// will also recieve an event (and the data that is held by the event).
-/// 
+///
 /// ## Type Parameters
-/// 
+///
 /// * `Input` - The type of data that is held by the event.
-/// 
+///
 /// ## Methods
-/// 
+///
 /// * `on_event` - Called when the event bus is run.
 pub trait Subscriber {
     /// The type of data that is held by the event.
